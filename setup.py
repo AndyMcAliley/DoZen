e3e0738e83967cd2808f42329f9122ddee01e142
Andy McAliley <andy.mcaliley@gmail.com>
2020-05-04 12:14:34 -0600

    Include DoZen in environment.yml

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
+install_requires = ['pip','numpy','pandas','scipy','pyproj==1.9.6']
+extras_requires = ['matplotlib','jupyter','pyviz==0.10']
+
 setup(
     name='DoZen',
     version='0.1',
     description='Load, process, plot, and QC electromagnetic data from Zonge z3d files',
-    url='https://git.multiphysics-mva.org/dozen.git',
+    url='https://github.com/AndyMcAliley/DoZen.git',
     author='Andy McAliley',
     author_email='andy.mcaliley@gmail.com',
     packages=['dozen',],
     license='MIT license',
-    # install_requires = ['pip','numpy','pandas','scipy','matplotlib','jupyter','pyviz==0.10','pyproj==1.9.6'],
+    install_requires = install_requires,
+    extras_requires = extras_requires,
     long_description=open('README.md').read(),
     zip_safe=False
 )
